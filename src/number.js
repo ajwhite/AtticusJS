26c08c816445f457246ffca4454a1d5622c7bdc1
Atticus White <contact@atticuswhite.com>
2014-05-09 13:13:57 -0400

    Number prototype extension

---
+++ src/number.js
@@ -0,0 +1,16 @@
+(function(){
+	"use strict";
+	
+	if (!Number.prototype.zeroPad){
+		Number.prototype.zeroPad = function(width){
+			var number = +this;
+			width -= number.toString().length;
+			
+			if (width > 0){
+				return new Array( width + (/\./.test(number) ? 2 : 1) ).join( '0' ) + number;
+			}
+			return number + "";
+		};
+	}
+	
+})();
