98f051a259f0b199c2d9d69d274a4f2d18eee25d
Atticus White <contact@atticuswhite.com>
2014-05-09 12:25:54 -0400

    extending date prototype

--- src/date.js
+++ src/date.js
@@ -1,26 +1,3 @@
-// Avoid console errors in unsupported browsers
-// Cred: OneMightyRoar
-(function(){
-	var method;
-	var noop = function(){};
-	var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-})();
 // Extending the Date prototype
 (function(){
 	
@@ -105,50 +82,3 @@
 	
 	
 })();
-// Handle input placeholders for unsupported browsers
-(function($){
-	"use strict";
-	
-	if (!$) return;
-	
-	
-	var PlaceholderUpdater = (function(){
-		function PlaceholderUpdater($el){
-			this.$el = $el;
-			this.placeholder = $el.attr('placeholder').trim();
-			
-			var _this = this;
-			$el.on('blur', function(){
-				_this.blur.call(_this);
-			});
-			$el.on('focus', function(){
-				_this.focus.call(_this);
-			});
-		}
-		
-		PlaceholderUpdater.prototype.blur = function(){
-			var val = this.$el.val().trim();
-			if (val.length === 0){
-				this.$el.val(this.placeholder);
-			}
-		};
-		
-		PlaceholderUpdater.prototype.focus = function(){
-			var val = this.$el.val().trim();
-			if (val == this.placeholder){
-				$(this).val('');
-			}
-		};
-		
-		return PlaceholderUpdater;
-	})();
-	
-	
-	$.fn.updatePlaceholders = function(){
-		this.each(function(){
-			return new PlaceholderUpdater($(this));
-		});
-	};
-	
-	
-})(jQuery || false);
