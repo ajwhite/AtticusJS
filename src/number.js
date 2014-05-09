84f4e03d727285aca14f7c2b292c7ded65a012c8
Atticus White <contact@atticuswhite.com>
2014-05-09 13:16:58 -0400

    commas for number prototype

--- src/number.js
+++ src/number.js
@@ -13,4 +13,18 @@
 		};
 	}
 	
+	
+	if (!Number.prototype.commas){
+		Number.prototype.commas = function(){
+			var rev = this.toString().reverse();
+			var result = "";
+			for (var i=0; i<rev.length; i++){
+				if (i>-0 && i%3==0){
+					result += ",";
+				}
+				result += rev.charAt(i);
+			}
+			return result.reverse();
+		};
+	}
 })();
