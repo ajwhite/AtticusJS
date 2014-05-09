b10a5415070908c8992f62d3fa491bc0e0214717
Atticus White <contact@atticuswhite.com>
2014-05-09 12:42:26 -0400

    extending math prototype

---
+++ src/math.js
@@ -0,0 +1,37 @@
+(function(){
+	"use strict";
+	
+	if (!Math.randomInt){
+		Math.randomInt = function(min, max){
+			return Math.floor(Math.random() * (max-min + +1)) + min;
+		};
+	}
+	
+	if (!Math.randomIntExcluding){
+		Math.randomIntExcluding = function(min, max, excluding){
+			excluding = Array.toArray(excluding);
+			var num;
+			
+			do {
+				num = Math.randomInt(min, max);
+			} while (excluding.contains(num));
+			
+			return num;
+		};
+	}
+	
+	if (!Math.degreesToRadians){
+		Math.degreesToRadians = function(degrees){
+			return degrees * (Math.PI/180);
+		};
+	}
+	
+	if (!Math.radiansToDegrees){
+		Math.radiansToDegrees = function(radians){
+			return radians * (180/Math.PI);
+		};
+	}
+	
+	
+	
+})();
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+(function(){
+	"use strict";
+	
+	window.getUrlVars = function(){
+		var vars = {}, hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf("?")+1).split("&");
+		
+		for (var i=0; i<hashes.length; i++){
+			hash = hashes[i].split("=");
+			vars[hash[0]] = hash[1];
+		}
+		
+		return vars;
+	};
+	
+	window.preloadImages = function(){
+		if (document.images){
+			for (var i=0; i<preloadImages.arguments.length; i++){
+				(new Image()).src = preloadImages.arguments[i];
+			}
+		}
+	};
+	
+	
+	
+	
+})();
